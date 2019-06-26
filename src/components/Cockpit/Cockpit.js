9797c17db387bb5d50f760ead2d33eacaef56a71
adrianjustinmartinez@gmail.com <adrianjustinmartinez@gmail.com>
2019-06-26 15:56:38 -0700

    useEffect hook usage

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import classes from './Cockpit.css';
 
 
 const cockpit = (props) => {
+    useEffect(() => {
+      console.log('[Cockpit.js] useEffect');
+      // Http request...
+      setTimeout(() => {
+        alert('Saved data to cloud!');
+      }, 1000);
+      return () => { //this will run before the regular useEffect function, but after the first render cycle
+        console.log('[Cockpit.js] cleanup work in useEffect');
+      };
+    }, []); // empty array means no dependencies making use effect only execute once at start
+    // [props.persons] Will only effect on persons property changing..
+
+    useEffect(() => {
+      console.log('[Cockpit.js] 2nd useEffect');
+      return () => { //this will run before the regular useEffect function, but after the first render cycle
+        console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+      }
+    }); // no arguments causes useEffect to run every render cycle
+    //useEffect() could do multiple for multiple changed properties
+
     const assignedClasses = [];
     let btnClass = '';
     if ( props.showPersons) {
--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,8 @@ class App extends Component {
       {id: 'asgads', name: 'Stephanie', age: 26}
     ],
     otherState: 'some other value',
-    showPersons: false
+    showPersons: false,
+    showCockpit: true
   }
 
   //React lifestyle hook
@@ -90,10 +91,12 @@ class App extends Component {
 
     return (
         <div className={classes.App}>
-          <Cockpit 
+          <button onClick={() => this.setState({showCockpit: false})}>Remove Cockpit</button>
+          {this.state.showCockpit ? <Cockpit 
             showPersons={this.state.showPersons}
             persons={this.state.persons}
             clicked={this.togglePersonsHandler}/>
+            : null }
           {persons}
         </div>
     );
