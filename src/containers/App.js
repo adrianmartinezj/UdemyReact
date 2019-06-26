304101fff1a4949189d2a304dee670fe4f2b0707
adrianjustinmartinez@gmail.com <adrianjustinmartinez@gmail.com>
2019-06-26 15:38:44 -0700

    Lifestyle and update hooks continued

--- src/containers/App.js
+++ src/containers/App.js
@@ -36,6 +36,16 @@ class App extends Component {
     console.log('[App.js] componentDidMount');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    // return false; straight up breaks the update cycle
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
+
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
